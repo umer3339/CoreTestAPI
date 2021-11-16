69c5fc644fab7b2805d8b1f0eac7e1ac971d1abf
arunbagri87 <60599378+arunbagri87@users.noreply.github.com>
2021-11-16 12:46:42 +0530

    COnfiguration Changess

--- STCAPI.Core.Entities/Configuration/ConfigurationMaster.cs
+++ STCAPI.Core.Entities/Configuration/ConfigurationMaster.cs
@@ -6,14 +6,14 @@ namespace STCAPI.Core.Entities.Configuration
     [Table("ConfigurationMaster")]
     public class ConfigurationMaster: BaseModel<int>
     {
-        public int MasterType { get; set; }
+        public string MasterType { get; set; }
         public int StageId { get; set; }
         public int MainStreamId { get; set; }
         public int StreamId { get; set; }
         public string Name { get; set; }
         public string LongName { get; set; }
         public string ShortName { get; set; }
-        public string ReportNumber { get; set; }
+        public decimal ReportNumber { get; set; }
         public string Description { get; set; }
         public string ConfigurationType { get; set; }
     }
